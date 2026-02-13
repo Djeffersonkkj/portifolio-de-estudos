cbc337d5cf93d5dad4097d69a82a41550249b931
Djefferson <djefferson141005@gmail.com>
2026-02-13 10:42:15 -0300

    POO | Iniciando estudos pelos materiais enviados por Danyel

---
+++ POO/DanyelsMaterial/Program.cs
@@ -0,0 +1,5 @@
+﻿Carro carro = new Carro();
+Console.WriteLine(carro.Acelerar(60));
+Console.WriteLine(carro.Desacelerar(50));
+Console.WriteLine(carro.Desacelerar(0));
+Console.WriteLine(carro.Desacelerar(11));
