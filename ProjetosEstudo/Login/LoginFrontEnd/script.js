8ef30cdf4bd0d2179965b38c2c2867d363b9d600
Djefferson <djefferson141005@gmail.com>
2026-02-20 10:42:15 -0300

    JS | Iniciando DOM

---
+++ ProjetosEstudo/Login/LoginFrontEnd/script.js
@@ -0,0 +1,8 @@
+var BotaoEntrar = document.getElementsByClassName("criarconta")[0];
+var BotaoCriar = document.getElementById("BotaoCriar");
+BotaoCriar.addEventListener("click", ClicarLogin);
+BotaoEntrar.addEventListener("click", ClicarLogin);
+
+function ClicarLogin(){
+    alert("Este botão ainda não funciona");
+}
